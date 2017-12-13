65e0cc369eb3129e111453c9e2368fe36abaa2d9
lianghanzhong <wyattliang@gmail.com>
2017-12-13 18:20:51 +0800

    :construction: 添加 Widget 基类

---
+++ components/WidgetBase.vue
@@ -0,0 +1,44 @@
+<template>
+  <div :style="styleObject">
+    <slot>empty widget</slot>
+  </div>
+</template>
+
+<script>
+function gridSizeToViewport(size) {
+  return Math.trunc(size) * 100 / 24
+}
+
+export default {
+  name: 'widget',
+  props: {
+    widgetX: {
+      type: Number,
+      default: 0,
+    },
+    widgetWidth: {
+      type: Number,
+      default: 6,
+    },
+    widgetY: {
+      type: Number,
+      default: 0,
+    },
+    widgetHeight: {
+      default: 6,
+      type: Number
+    },
+  },
+  data () {
+    return {
+      styleObject: {
+        position: 'absolute',
+        width: gridSizeToViewport(this.widgetWidth) +'vw',
+        height: gridSizeToViewport(this.widgetHeight) +'vh',
+        left: gridSizeToViewport(this.widgetX) +'vw',
+        top: gridSizeToViewport(this.widgetY) +'vh',
+      }
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,25 @@
 <template>
+  <widget-base :widget-x="6" :widget-y="6" :widget-width="12" :widget-height="12">
   <el-card class="box-card">
     <h1>It Works</h1>
   </el-card>
+  </widget-base>
 </template>
+
+<script>
+import WidgetBase from '~/components/WidgetBase'
+
+export default {
+  name: 'index',
+  components: {
+    'widget-base': WidgetBase,
+  },
+}
+</script>
 
 <style>
   .box-card {
-    width: 480px;
+    width: 100%;
+    height: 100%;
   }
 </style>
